1f4813c34f8b04059f7f77095e4054ce992084ca
wanghuajie <wanghuajie@csvw.com>
2020-06-14 19:03:29 +0800

    L1 Project Create

---
+++ L1/Homework/Cal2to100.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+x1 = np.arange(2,101,2)
+#print(x1)
+sum = x1.sum()
+print(sum)
---
+++ L1/Homework/ClassStatics.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+result = pd.read_csv('class.csv')
+print(result)
+
+print(result.describe())
